68a90eb8e61a7c9936992c795cabb26d852bc358
ChiaraTorta <chiara.torta@mailbox.org>
2022-04-29 13:51:39 +0200

    init version of latest photos data

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -2,7 +2,7 @@ let store = {
     user: { name: "Student" },
     apod: '',
     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-    photos: ''
+    mars_photos: []
 }
 
 // add our markup to the page
@@ -20,27 +20,14 @@ const render = async (root, state) => {
 
 // create content
 const App = (state) => {
-    let { rovers, photos, apod } = state
+    let { rovers, mars_photos, apod } = state
 
     return `
         <header></header>
         <main>
             ${Greeting(store.user.name)}
             <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
-            </section>
-            <section>
-                ${Gallery(rovers, photos)}
+                ${LatestPhotos(mars_photos)}
             </section>
         </main>
         <footer></footer>
@@ -67,56 +54,24 @@ const Greeting = (name) => {
     `
 }
 
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
-
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
-
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
+const LatestPhotos = (mars_photos) => {
+    // If gallery mars_photos don't already exist -- request them again
+    if (mars_photos.length < 1 || mars_photos === undefined) {
+        getMarsPhotos(store)
     }
 
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
+    return (`
+            <img src="${mars_photos.photos.latest_photos[0].img_src}" height="350px" width="100%" />
+            <p>${mars_photos.photos.latest_photos[0].rover.name}</p>
         `)
-    } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `)
-    }
-}
-
-const Gallery = (rovers, photos) => {
-    // If gallery photos don't already exist -- request them again
-    if (!photos) {
-        getRoverPhotos(rovers)
-    }
 }
 
 // ------------------------------------------------------  API CALLS
 
-const getRoverPhotos = (rovers) => {
-    rovers.map(rover => fetch(`http://localhost:3000/rovers/${rover}`)
+const getMarsPhotos = (state) => {
+    let { mars_photos, rovers } = state
+
+    fetch(`http://localhost:3000/rovers/${rovers[0]}`)
     .then(res => res.json())
-    .then(data => updateStore(store, { data } )))
-}
-
-// Example API call
-const getImageOfTheDay = (state) => {
-    let { apod } = state
-
-    fetch(`http://localhost:3000/apod`)
-        .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
-
-    return data
+    .then(mars_photos => updateStore(store, { mars_photos } ))
 }
